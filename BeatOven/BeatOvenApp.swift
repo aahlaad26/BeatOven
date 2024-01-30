5835f428ff40f13b089c27a2e7ae20de535df176
aahlaad26 <aahlaad26092002@gmail.com>
2024-01-30 09:46:54 +0530

    Initial Commit

---
+++ BeatOven/BeatOvenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatOvenApp.swift
+//  BeatOven
+//
+//  Created by user2 on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BeatOvenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeatOven/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeatOven
+//
+//  Created by user2 on 30/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
